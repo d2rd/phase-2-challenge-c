e0ffdfbff62531263b0501db5925278e86015ed1
Glenn-David Daniel <d2rd@outlook.com>
2017-08-15 23:30:31 -0700

    searchInYears working

--- part-2/searchInYears.js
+++ part-2/searchInYears.js
@@ -10,17 +10,21 @@
 // 1. Get required resources
     var fs = require('fs'); //requires the node 'fs' library.
     var carData = require('./cars');
-    var targetModel = process.argv[2]; //search parameter is the 3rd array item.
-// console.log(targetModel + " in targetModel");
+    var targetYears = process.argv[2]; //search parameter is the 3rd array item.
+// console.log(targetYears + " in targetYears");
 
 // 2. load the cars.json file into the var 'carData' (using absolute path).
-    const carArray = JSON.parse(fs.readFileSync('./cars.json', 'utf8')); 
+    const carArray = JSON.parse(fs.readFileSync('./cars.json', 'utf8')); //using JSON.parse to convert JSON string into JSON object (review chat with Bonnie)
 
-// 3. Declare helper function 'inYears' to match search parameter 'targetModel' to array values.
+// 3. Declare helper function 'inYears' to match search parameter 'targetYears' to array values.
 // Attribution:  https://www.youtube.com/watch?v=q_MXH_Ponpg
     function inYears(carObj, index, carData) { 
-        // console.log(carObj + " in carObj");   
-      if (carObj.model == targetModel) {
+        // console.log(carObj + " in carObj");
+//SPLIT var 'targetYears' into array 'targetYearsArray'
+    targetYearsArray = targetYears.split("-");
+
+     
+      if (carObj.year >= targetYearsArray[0] && carObj.year <= targetYearsArray[1]) {
             return true;
         }
         else {
@@ -31,8 +35,9 @@
 
 var carsFound = carArray.filter(inYears); // NOTE: inYears IS the callback.  runs filter method against new array 'carsFound' containing matches output by 'inYears' helper function.
 // console.log(JSON.parse(carsFound));  // converts JSON string to object.
-console.log(carsFound + " in carsFound"); // print all the values of carsFound
+// console.log(carsFound + " in carsFound"); // print all the values of carsFound
 
+console.log("Finding cars from " + targetYearsArray[0] + " to " + targetYearsArray[1] + " inclusive.");
 for (i in carsFound){
     // console.log(i + " in 'i'");
     for (key in carsFound[i]){
